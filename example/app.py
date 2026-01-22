04c90bffaf71884e50b2ec7b65d417ff1e3bf71c
Eduard M <4310497+edisile@users.noreply.github.com>
2026-01-22 11:36:51 +0100

    feature: flask-talisman in example app

--- example/app.py
+++ example/app.py
@@ -1,4 +1,5 @@
 import flask
+from flask_talisman import Talisman
 from canonicalwebteam.flask_vite import FlaskVite
 
 app = flask.Flask(__name__, template_folder="templates")
@@ -6,6 +7,21 @@ app = flask.Flask(__name__, template_folder="templates")
 app.config["VITE_MODE"] = "development" if app.debug else "production"
 app.config["VITE_PORT"] = 9999
 app.config["VITE_OUTDIR"] = "static/vite/dist"
+
+csp = {
+    "default-src": ["'self'"],
+    "script-src": ["'self'"],
+}
+
+if app.debug:
+    csp["connect-src"] = ["'self'", "ws://localhost:9999"]
+
+talisman = Talisman(
+    app,
+    content_security_policy=csp,
+    content_security_policy_nonce_in=["script-src", "default-src"],
+    force_https=False,
+)
 
 FlaskVite(app)
 
